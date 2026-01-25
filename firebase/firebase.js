fe237e91481768d64c52b491e4121f999440da21
Abhinav Vishist <avi@Abhinavs-MacBook-Air.local>
2026-01-26 01:02:37 +0530

    Add Firebase config and auth setup

---
+++ firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDWBNMvcuS7CjBiKBMISR0qWYclXoOIE5s",
+    authDomain: "pitchpdf-9bd30.firebaseapp.com",
+    projectId: "pitchpdf-9bd30",
+    storageBucket: "pitchpdf-9bd30.firebasestorage.app",
+    messagingSenderId: "346968130855",
+    appId: "1:346968130855:web:b6e4237bce8f763ea4d502"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
